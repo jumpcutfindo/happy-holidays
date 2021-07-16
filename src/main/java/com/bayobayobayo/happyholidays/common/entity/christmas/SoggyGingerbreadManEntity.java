7cdc99e63a287df560f7a5182af32b2eacf3864d
Daniel Hoe <danielhoe97@gmail.com>
2021-07-16 17:17:20 +0800

    Make soggy gingerbread man drop items on conversion

--- src/main/java/com/bayobayobayo/happyholidays/common/entity/christmas/SoggyGingerbreadManEntity.java
+++ src/main/java/com/bayobayobayo/happyholidays/common/entity/christmas/SoggyGingerbreadManEntity.java
@@ -2,25 +2,16 @@ package com.bayobayobayo.happyholidays.common.entity.christmas;
 
 import com.bayobayobayo.happyholidays.common.handlers.ModuleHandler;
 
-import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.CryingObsidianBlock;
-import net.minecraft.block.FurnaceBlock;
-import net.minecraft.block.MagmaBlock;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntitySize;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.monster.piglin.PiglinEntity;
-import net.minecraft.entity.passive.WolfEntity;
-import net.minecraft.particles.BlockParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.world.Dimension;
 import net.minecraft.world.World;
 
 public class SoggyGingerbreadManEntity extends GingerbreadPersonEntity {
@@ -37,25 +28,26 @@ public class SoggyGingerbreadManEntity extends GingerbreadPersonEntity {
     @Override
     public void tick() {
         super.tick();
-        if (!this.level.isClientSide() && isNearFireSource()) {
-            spawnSmokeParticles();
-
+        if (!this.level.isClientSide() && !isInWaterOrRain() && isNearFireSource()) {
             if (--timeLeftToDry == 0.0f) convertToDry();
+        }
+
+        if (this.level.isClientSide() && !isInWaterOrRain() && isNearFireSource()) {
+            spawnSmokeParticles();
         }
     }
 
     private void spawnSmokeParticles() {
-        EntitySize dimensions = this.getDimensions(this.getPose());
-        Vector3d vector3d = this.getDeltaMovement();
+        EntitySize size = this.getDimensions(this.getPose());
 
         this.level.addParticle(
                 ParticleTypes.SMOKE,
-                this.getX() + (this.random.nextDouble() - 0.5D) * (double) dimensions.width,
-                this.getY() + (this.random.nextDouble() * 2.0D),
-                this.getZ() + (this.random.nextDouble() - 0.5D) * (double) dimensions.width,
-                vector3d.x * 4.0D,
-                0.5D,
-                vector3d.z * 4.0D
+                this.getX() + random.nextDouble() * size.width - 0.5D,
+                this.getY() + random.nextDouble() * size.height,
+                this.getZ() + random.nextDouble() * size.width - 0.5D,
+                0.0D,
+                0.0D,
+                0.0D
         );
     }
 
@@ -76,7 +68,8 @@ public class SoggyGingerbreadManEntity extends GingerbreadPersonEntity {
                 || blockState.is(Blocks.SMOKER) && blockState.getValue(BlockStateProperties.LIT)
                 || blockState.is(Blocks.FIRE) || blockState.is(Blocks.SOUL_FIRE)
                 || blockState.is(Blocks.CAMPFIRE) || blockState.is(Blocks.SOUL_CAMPFIRE)
-                || blockState.is(Blocks.MAGMA_BLOCK)) {
+                || blockState.is(Blocks.MAGMA_BLOCK)
+            ) {
                 return true;
             }
         }
@@ -88,8 +81,9 @@ public class SoggyGingerbreadManEntity extends GingerbreadPersonEntity {
         GingerbreadEntities gingerbreadEntities =
                 (GingerbreadEntities) ModuleHandler.CHRISTMAS_HANDLER.getSpecificEntities(GingerbreadEntities.GINGERBREAD_ENTITIES_ID);
 
-
         this.convertTo(gingerbreadEntities.getGingerbreadManObject().get(), true);
         this.playSound(SoundEvents.FIRE_EXTINGUISH, 1.0F, 1.0F);
+
+        this.dropFromLootTable(DamageSource.DROWN, true);
     }
 }
