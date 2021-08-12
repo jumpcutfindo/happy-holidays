3d962cc335ee62767ac6994a4b0b6e8f0893568b
Daniel Hoe <danielhoe97@gmail.com>
2021-08-12 16:04:59 +0800

    Adjust growth probability of presents

--- src/main/java/com/jumpcutfindo/happyholidays/common/block/christmas/presents/PresentBlock.java
+++ src/main/java/com/jumpcutfindo/happyholidays/common/block/christmas/presents/PresentBlock.java
@@ -57,7 +57,7 @@ public class PresentBlock extends ChristmasBlock {
                     .stacksTo(16)
                     .tab(ModuleHandler.HAPPY_HOLIDAYS_GROUP);
 
-    public static final float GROWTH_PROBABILITY = 1.0f / 128.0f;
+    public static final float GROWTH_PROBABILITY = 1.0f / 64.0f;
 
     private VoxelShape shape;
 
