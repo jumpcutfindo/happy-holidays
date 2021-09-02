5a1adaaee5bbe0306a6715da5ae848e0eaad1788
jumpcutfindo <danielhoe97@gmail.com>
2021-09-02 17:19:53 +0800

    Change naughty nice meter to not update when santa killed

--- src/main/java/com/jumpcutfindo/happyholidays/common/events/christmas/NaughtyNiceEvents.java
+++ src/main/java/com/jumpcutfindo/happyholidays/common/events/christmas/NaughtyNiceEvents.java
@@ -9,6 +9,7 @@ import com.jumpcutfindo.happyholidays.common.capabilities.christmas.INaughtyNice
 import com.jumpcutfindo.happyholidays.common.capabilities.christmas.NaughtyNiceAction;
 import com.jumpcutfindo.happyholidays.common.capabilities.christmas.NaughtyNiceMeter;
 import com.jumpcutfindo.happyholidays.common.entity.christmas.elf.SantaElfEntity;
+import com.jumpcutfindo.happyholidays.common.entity.christmas.santa.BaseSantaEntity;
 
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.LivingEntity;
@@ -20,13 +21,6 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = HappyHolidaysMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class NaughtyNiceEvents {
-    public static final int NICE_KILL_HOSTILE_MOB_POINTS = 10;
-    public static final int NICE_HELP_SANTA_ELF_POINTS = 20;
-    public static final int NICE_HELP_GRINCH_POINTS = 20;
-
-    public static final int NAUGHTY_KILL_PASSIVE_MOB_POINTS = 5;
-    public static final int NAUGHTY_KILL_SANTA_ELF_POINTS = 200;
-
     /**
      * Replicates the naughty-nice meter levels on player's death.
      * @param event Player death event.
@@ -64,7 +58,8 @@ public class NaughtyNiceEvents {
                 if (killedEntity instanceof SantaElfEntity) NaughtyNiceMeter.evaluateAction(player, NaughtyNiceAction.KILL_SANTA_ELF_EVENT);
                 else NaughtyNiceMeter.evaluateAction(player, NaughtyNiceAction.KILL_PASSIVE_MOB_EVENT);
             } else {
-                NaughtyNiceMeter.evaluateAction(player, NaughtyNiceAction.KILL_HOSTILE_MOB_EVENT);
+                if (!(killedEntity instanceof BaseSantaEntity)) NaughtyNiceMeter.evaluateAction(player,
+                        NaughtyNiceAction.KILL_HOSTILE_MOB_EVENT);
             }
         }
     }
