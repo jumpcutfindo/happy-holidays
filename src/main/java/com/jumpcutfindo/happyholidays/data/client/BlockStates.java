fdb914289ddf4c14f6bf7f364e1354490944e3e8
jumpcutfindo <danielhoe97@gmail.com>
2021-10-14 17:36:27 +0800

    Add datagen for basic ornament blocks

--- src/main/java/com/jumpcutfindo/happyholidays/data/client/BlockStates.java
+++ src/main/java/com/jumpcutfindo/happyholidays/data/client/BlockStates.java
@@ -1,12 +1,18 @@
 package com.jumpcutfindo.happyholidays.data.client;
 
+import java.util.Set;
+
 import com.jumpcutfindo.happyholidays.HappyHolidaysMod;
+import com.jumpcutfindo.happyholidays.common.block.christmas.decorations.OrnamentBlock;
 import com.jumpcutfindo.happyholidays.common.registry.christmas.ChristmasBlocks;
 
+import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -17,7 +23,87 @@ public class BlockStates extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        this.simpleBlock(ChristmasBlocks.GINGERBREAD_BLOCK.get());
+        registerOrnaments();
+    }
+
+    private void registerOrnaments() {
+        Set<Block> blocks = Set.of(
+                ChristmasBlocks.BABY_PRESENT_ORNAMENT.get(),
+                ChristmasBlocks.ADULT_PRESENT_ORNAMENT.get(),
+                ChristmasBlocks.ELDER_PRESENT_ORNAMENT.get(),
+                ChristmasBlocks.CANDY_CANE_ORNAMENT.get(),
+                ChristmasBlocks.SANTA_ELF_ORNAMENT.get(),
+                ChristmasBlocks.GINGERBREAD_MAN_ORNAMENT.get(),
+                ChristmasBlocks.GRINCH_ORNAMENT.get(),
+
+                ChristmasBlocks.RED_BAUBLE.get(),
+                ChristmasBlocks.BLUE_BAUBLE.get(),
+                ChristmasBlocks.YELLOW_BAUBLE.get(),
+                ChristmasBlocks.GREEN_BAUBLE.get(),
+                ChristmasBlocks.GOLD_BAUBLE.get(),
+                ChristmasBlocks.SILVER_BAUBLE.get(),
+
+                ChristmasBlocks.BIG_RED_BAUBLE.get(),
+                ChristmasBlocks.BIG_BLUE_BAUBLE.get(),
+                ChristmasBlocks.BIG_YELLOW_BAUBLE.get(),
+                ChristmasBlocks.BIG_GREEN_BAUBLE.get(),
+                ChristmasBlocks.BIG_GOLD_BAUBLE.get(),
+                ChristmasBlocks.BIG_SILVER_BAUBLE.get(),
+
+                ChristmasBlocks.CREEPER_HEAD_ORNAMENT.get(),
+                ChristmasBlocks.SKELETON_HEAD_ORNAMENT.get(),
+                ChristmasBlocks.WITHER_SKELETON_HEAD_ORNAMENT.get(),
+                ChristmasBlocks.ZOMBIE_HEAD_ORNAMENT.get(),
+
+                ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_A.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_B.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_C.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_D.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_E.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_F.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_G.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_H.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_I.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_J.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_K.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_L.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_M.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_N.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_O.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_P.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_Q.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_R.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_S.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_T.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_U.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_V.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_W.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_X.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_Y.get(),
+                ChristmasBlocks.ALPHABET_ORNAMENT_Z.get()
+        );
+
+        for (Block block : blocks) ornamentOf(block);
+    }
+
+    private void ornamentOf(Block block) {
+        String blockId = blockId(block);
+
+        getVariantBuilder(block).forAllStatesExcept(state -> {
+            ConfiguredModel.Builder<?> builder = ConfiguredModel.builder();
+
+            Direction facingDirection = state.getValue(OrnamentBlock.FACING);
+            AttachFace attachFace = state.getValue(OrnamentBlock.ATTACH_FACE);
+
+            switch (attachFace) {
+            case WALL -> builder = builder.modelFile(modelFileOf(blockId + "_side"));
+            case CEILING -> builder = builder.modelFile(modelFileOf(blockId + "_hanging"));
+            case FLOOR -> builder = builder.modelFile(modelFileOf(blockId));
+            }
+
+            return builder.rotationY(getRotationY(facingDirection)).build();
+        }, OrnamentBlock.WATERLOGGED);
     }
 
     private String blockId(Block block) {
@@ -28,7 +114,20 @@ public class BlockStates extends BlockStateProvider {
         return new ResourceLocation(HappyHolidaysMod.MOD_ID, "block/" + id);
     }
 
+    private ModelFile modelFileOf(String modelId) {
+        return new ModelFile.ExistingModelFile(resourceOfBlock(modelId), models().existingFileHelper);
+    }
+
     private ModelFile modelFileOf(Block block) {
         return new ModelFile.ExistingModelFile(resourceOfBlock(blockId(block)), models().existingFileHelper);
+    }
+
+    private int getRotationY(Direction direction) {
+        return switch (direction) {
+            case EAST -> 270;
+            case NORTH -> 180;
+            case WEST -> 90;
+            default -> 0;
+        };
     }
 }
