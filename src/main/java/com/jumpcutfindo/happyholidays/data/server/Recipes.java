ebba8583c9842ecfd0a7708820c1590570057b79
jumpcutfindo <danielhoe97@gmail.com>
2021-10-15 23:25:42 +0800

    Add data generator for alphabet ornaments, candy cane items

--- src/main/java/com/jumpcutfindo/happyholidays/data/server/Recipes.java
+++ src/main/java/com/jumpcutfindo/happyholidays/data/server/Recipes.java
@@ -11,10 +11,12 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.data.recipes.SingleItemRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 
@@ -74,11 +76,36 @@ public class Recipes extends RecipeProvider {
         stocking(consumer, ChristmasBlocks.GOLD_STOCKING.get(), ChristmasItems.GOLD_CHRISTMAS_DYE.get());
         stocking(consumer, ChristmasBlocks.SILVER_STOCKING.get(), ChristmasItems.SILVER_CHRISTMAS_DYE.get());
 
-        ShapedRecipeBuilder.shaped(ChristmasBlocks.BABY_PRESENT.get(), 2)
-                .define('S', Items.STRING).define('#', ChristmasItems.PRESENT_SCRAPS.get()).define('W', ItemTags.WOOL)
-                .pattern("S#S").pattern("WWW")
-                .unlockedBy("has_scraps", has(ChristmasItems.PRESENT_SCRAPS.get())).unlockedBy("has_string", has(Items.STRING)).unlockedBy("has_wool", has(ItemTags.WOOL))
-                .save(consumer, recipeResourceOf(ChristmasBlocks.BABY_PRESENT.get()));
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get(), Items.GLASS, 8);
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_A.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_B.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_C.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_D.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_E.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_F.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_G.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_H.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_I.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_J.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_K.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_L.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_M.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_N.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_O.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_P.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_Q.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_R.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_S.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_T.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_U.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_V.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_W.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_X.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_Y.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+        stonecutterResultFromBase(consumer, ChristmasBlocks.ALPHABET_ORNAMENT_Z.get(), ChristmasBlocks.ALPHABET_ORNAMENT_TEMPLATE.get());
+
+        babyPresent(consumer);
+        candyCane(consumer);
     }
 
     private void dyeBowl(Consumer<FinishedRecipe> consumer) {
@@ -140,6 +167,70 @@ public class Recipes extends RecipeProvider {
                 .pattern("S").pattern("W").pattern("A")
                 .unlockedBy("has_string", has(Items.STRING)).unlockedBy("has_wool", has(ItemTags.WOOL))
                 .save(consumer, recipeResourceOf(block));
+    }
+
+    private void babyPresent(Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(ChristmasBlocks.BABY_PRESENT.get(), 2)
+                .define('S', Items.STRING).define('#', ChristmasItems.PRESENT_SCRAPS.get()).define('W', ItemTags.WOOL)
+                .pattern("S#S").pattern("WWW")
+                .unlockedBy("has_scraps", has(ChristmasItems.PRESENT_SCRAPS.get())).unlockedBy("has_string", has(Items.STRING)).unlockedBy("has_wool", has(ItemTags.WOOL))
+                .save(consumer, recipeResourceOf(ChristmasBlocks.BABY_PRESENT.get()));
+    }
+
+    private void candyCane(Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(ChristmasBlocks.CANDY_CANE_BLOCK.get(), 1)
+                .define('#', ChristmasItems.CANDY_CANE.get())
+                .pattern("##").pattern("##")
+                .unlockedBy(getHasName(ChristmasItems.CANDY_CANE.get()), has(ChristmasItems.CANDY_CANE.get()))
+                .save(consumer, recipeResourceOf(ChristmasBlocks.CANDY_CANE_BLOCK.get()));
+
+        ShapedRecipeBuilder.shaped(ChristmasBlocks.CANDY_CANE_BLOCK.get(), 4)
+                .define('#', Items.SUGAR).define('R', Items.RED_DYE)
+                .pattern("R#").pattern("#R")
+                .unlockedBy(getHasName(Items.SUGAR), has(Items.SUGAR)).unlockedBy(getHasName(Items.RED_DYE), has(Items.RED_DYE))
+                .save(consumer, recipeResourceOf(ChristmasBlocks.CANDY_CANE_BLOCK.get()) + "_from_raw_items");
+
+        ShapedRecipeBuilder.shaped(ChristmasBlocks.FESTIVE_CANDY_CANE_BLOCK.get(), 1)
+                .define('#', ChristmasItems.FESTIVE_CANDY_CANE.get())
+                .pattern("##").pattern("##")
+                .unlockedBy(getHasName(ChristmasItems.FESTIVE_CANDY_CANE.get()), has(ChristmasItems.FESTIVE_CANDY_CANE.get()))
+                .save(consumer, recipeResourceOf(ChristmasBlocks.FESTIVE_CANDY_CANE_BLOCK.get()));
+
+        ShapedRecipeBuilder.shaped(ChristmasBlocks.FESTIVE_CANDY_CANE_BLOCK.get(), 4)
+                .define('#', Items.SUGAR).define('R', Items.RED_DYE).define('G', Items.GREEN_DYE)
+                .pattern("R#").pattern("#G")
+                .unlockedBy(getHasName(Items.SUGAR), has(Items.SUGAR)).unlockedBy(getHasName(Items.RED_DYE), has(Items.RED_DYE)).unlockedBy(getHasName(Items.GREEN_DYE), has(Items.GREEN_DYE))
+                .save(consumer, recipeResourceOf(ChristmasBlocks.FESTIVE_CANDY_CANE_BLOCK.get()) + "_from_raw_items");
+
+        ShapelessRecipeBuilder.shapeless(ChristmasItems.CANDY_CANE_ORNAMENT.get(), 1)
+                .requires(Items.STRING).requires(ChristmasItems.ENCHANTED_CANDY_CANE.get())
+                .unlockedBy(getHasName(ChristmasItems.ENCHANTED_CANDY_CANE.get()), has(ChristmasItems.ENCHANTED_CANDY_CANE.get()))
+                .save(consumer, recipeResourceOf(ChristmasItems.CANDY_CANE_ORNAMENT.get()));
+
+        ShapelessRecipeBuilder.shapeless(ChristmasItems.SANTA_ELF_BELL.get(), 1)
+                .requires(Items.BELL).requires(ChristmasItems.ENCHANTED_CANDY_CANE.get())
+                .unlockedBy(getHasName(ChristmasItems.ENCHANTED_CANDY_CANE.get()), has(ChristmasItems.ENCHANTED_CANDY_CANE.get()))
+                .save(consumer, recipeResourceOf(ChristmasItems.SANTA_ELF_BELL.get()));
+    }
+
+    private void stonecutterResultFromBase(Consumer<FinishedRecipe> consumer, ItemLike result, ItemLike source) {
+        stonecutterResultFromBase(consumer, result, source, 1);
+    }
+
+    private void stonecutterResultFromBase(Consumer<FinishedRecipe> consumer, ItemLike result, ItemLike source, int count) {
+        SingleItemRecipeBuilder.stonecutting(Ingredient.of(source), result, count).unlockedBy(getHasName(source), has(source)).save(consumer, stonecutterRecipeResourceOf(result, source));
+    }
+
+    private ResourceLocation stonecutterRecipeResourceOf(ItemLike result, ItemLike source) {
+        return new ResourceLocation(HappyHolidaysMod.MOD_ID, itemId(result) + "_from_" + itemId(source) + "_stonecutting");
+    }
+
+    private String getHasName(ItemLike item) {
+        return "has_" + itemId(item);
+    }
+
+    private String itemId(ItemLike item) {
+        return item instanceof Item ? ((Item) item).getRegistryName().getPath() : ((Block) item).getRegistryName().getPath();
     }
 
     private ResourceLocation recipeResourceOf(ItemLike item) {
