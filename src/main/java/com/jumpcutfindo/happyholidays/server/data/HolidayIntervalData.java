e1f5b2f10e5f28aa764b7b4383cb80df4478b825
jumpcutfindo <danielhoe97@gmail.com>
2022-02-01 19:11:14 +0800

    Create holiday interval data class

---
+++ src/main/java/com/jumpcutfindo/happyholidays/server/data/HolidayIntervalData.java
@@ -0,0 +1,73 @@
+package com.jumpcutfindo.happyholidays.server.data;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.jumpcutfindo.happyholidays.HappyHolidaysMod;
+import com.jumpcutfindo.happyholidays.server.data.structs.Interval;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.saveddata.SavedData;
+
+public class HolidayIntervalData extends SavedData {
+    public static final Collection<String> HOLIDAY_CODES = List.of("christmas");
+    public static final String DATA_NAME = HappyHolidaysMod.MOD_ID + "_intervals";
+
+    private final Map<String, Interval> holidayIntervalMap = Map.ofEntries(
+            Map.entry("christmas", Interval.all())
+    );
+
+    public HolidayIntervalData() {
+    }
+
+    public void put(String holidayCode, Interval interval) {
+        holidayIntervalMap.put(holidayCode, interval);
+    }
+
+    @Override
+    public CompoundTag save(CompoundTag tag) {
+        for (String holidayCode : holidayIntervalMap.keySet()) {
+            CompoundTag holidayTag = new CompoundTag();
+
+            holidayTag.putInt("StartMonth", holidayIntervalMap.get("christmas").getStart().getMonth());
+            holidayTag.putInt("StartDay", holidayIntervalMap.get("christmas").getStart().getDay());
+            holidayTag.putInt("EndMonth", holidayIntervalMap.get("christmas").getEnd().getMonth());
+            holidayTag.putInt("EndDay", holidayIntervalMap.get("christmas").getEnd().getDay());
+
+            tag.put(StringUtils.capitalize(holidayCode), holidayTag);
+        }
+        return tag;
+    }
+
+    public static HolidayIntervalData createFromTag(CompoundTag tag) {
+        HolidayIntervalData holidayIntervalData = new HolidayIntervalData();
+
+        for (String holidayCode : HOLIDAY_CODES) {
+            String tagName = StringUtils.capitalize(holidayCode);
+            if (tag.contains(tagName)) {
+                CompoundTag holidayTag = tag.getCompound(tagName);
+
+                int startMonth = holidayTag.getInt("StartMonth");
+                int startDay = holidayTag.getInt("StartDay");
+                int endMonth = holidayTag.getInt("EndMonth");
+                int endDay = holidayTag.getInt("EndDay");
+
+                holidayIntervalData.put(holidayCode, Interval.of(startMonth, startDay, endMonth, endDay));
+            }
+        }
+
+        return holidayIntervalData;
+    }
+
+    public static HolidayIntervalData retrieve(ServerLevel serverLevel) {
+        return serverLevel.getDataStorage().computeIfAbsent(
+                HolidayIntervalData::createFromTag,
+                HolidayIntervalData::new,
+                HolidayIntervalData.DATA_NAME
+        );
+    }
+}
--- src/main/java/com/jumpcutfindo/happyholidays/server/data/structs/Interval.java
+++ src/main/java/com/jumpcutfindo/happyholidays/server/data/structs/Interval.java
@@ -1,6 +1,12 @@
 package com.jumpcutfindo.happyholidays.server.data.structs;
 
+import java.util.Map;
+
 public class Interval {
+    public static final Map<String, Interval> SEASONAL_HOLIDAY_INTERVALS = Map.ofEntries(
+            Map.entry("christmas", Interval.of(12, 1, 12, 31))
+    );
+
     private final YearlessDate start, end;
 
     public Interval(YearlessDate start, YearlessDate end) {
@@ -19,5 +25,13 @@ public class Interval {
     public boolean contains(YearlessDate current) {
         if (start.getMonth() > end.getMonth()) return current.isAfter(start) || current.isBefore(end);
         else return current.isAfter(start) && current.isBefore(end);
+    }
+
+    public static Interval of(int startMonth, int startDay, int endMonth, int endDay) {
+        return new Interval(new YearlessDate(startMonth, startDay), new YearlessDate(endMonth, endDay));
+    }
+
+    public static Interval all() {
+        return Interval.of(1, 1, 12, 31);
     }
 }
